8e9b8e6ee15530264daf00b5819fd6595f75b454
yurrko <yurrko@yandex.ru>
2018-04-19 22:42:02 +0300

    Списки

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -11,13 +11,27 @@ namespace Snake
         static void Main(string[] args)
         {
             Point p1 = new Point(1, 3, '*');
-            //p1.x = 1;
-            //p1.y = 3;
-            //p1.sym = '*';
             p1.Draw();
 
-            Point p2 = new Point();
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+
+            }
+
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
 
             Console.ReadLine();
         }
